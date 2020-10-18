e535aefc766fc79ae7fcaf0ae350b2fb7973fd2b
Shivam Srivastava <shivamsri142@gmail.com>
2020-10-18 18:20:06 +0530

    Update

--- snake.py
+++ snake.py
@@ -202,13 +202,3 @@ def game_loop():
     quit()
 
 welcome()
-
-# Colors codes:
-# Black = (0,0,0)
-# White = (255,255,255)
-# Medium Gray = (128,128,128)
-# Aqua = (0,128,128)
-# Navy Blue	= (0,0128)
-# Green = (0,255,0)
-# Orange = (255,165,0)
-# Yellow = (255,255,0)
